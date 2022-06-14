0465fae6b36547d128ecd62eed2096ca94cea04f
jobarv <jbarrera@vabaja.com.mx>
2022-06-13 19:42:02 -0500

    updates2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min"
+import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from 'react';
 import NavBar from './components/common/NavBar';
-import ItemCount from './components/common/ItemCount';
-import './App.css';
+// import ItemCount from './components/common/ItemCount';
+import ItemDetail from './components/common/ItemDetail';
+import ItemDetailCointainer from './components/common/ItemDetailCointainer';
 
 
 const App = () => {
-  return(
-<div>
-  <NavBar/>
-  <ItemCount/>
-</div>
-  )
+  return (
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/Producto/:id" element={<ItemDetailCointainer />} />
+          <Route path="/Nosotros" />
+          <Route path="/Contacto" />
+        </Routes>
+      </BrowserRouter>
+
+    </>
+  );
 }
 
 export default App;
--- src/components/common/ItemCount.jsx
+++ src/components/common/ItemCount.jsx
@@ -1,50 +1,52 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const ItemCount = () => {
-    const stock = 10;
-    //  Se establece el inicio del contador en "0"
-    const [counter, setCounter] = useState(0)
-    // Función Decremento
-    const decremento = () => {
-//  Si el contador llega a menos que "0" manda una alerta 
-        if ( counter < 1 ) {
-            alert ("Debes solicitar al menos 1 pieza") 
-        } else {
-            // El estado del contador decrementa el conteo
-            setCounter(counter - 1)
-        }
+  const stock = 10;
+  //  Se establece el inicio del contador en "0"
+  const [counter, setCounter] = useState(0);
+  // Función Decremento
+  const decremento = () => {
+    //  Si el contador llega a menos que "0" manda una alerta
+    if (counter < 1) {
+      alert("Debes solicitar al menos 1 pieza");
+    } else {
+      // El estado del contador decrementa el conteo
+      setCounter(counter - 1);
     }
+  };
 
-    // Función Incremento
-    const incremento = () => {
-        // Si el contador excede el número definido de piezas en stock, manda una alerta 
-        if (counter >= stock ) {
-
-            alert ("lo siento, solo tengo " + stock + " piezas");
-
-        } else {
-
-            // El estado del contador incrementa el conteo
-            setCounter(counter + 1)
-        }
+  // Función Incremento
+  const incremento = () => {
+    // Si el contador excede el número definido de piezas en stock, manda una alerta
+    if (counter >= stock) {
+      alert("lo siento, solo tengo " + stock + " piezas");
+    } else {
+      // El estado del contador incrementa el conteo
+      setCounter(counter + 1);
     }
+  };
 
-
-    return (
-        <div className="ItemCount">
-            <h2>Playera 1</h2>
-            <img src="" alt="" />
-            <div className="counter">
-                {counter}
-            </div>
-            <div className="buttons">
-                <button className="decremento" onClick={decremento}>-</button>
-                <button className="incremento" onClick={incremento}>+</button>
-            </div>
-            <h3>Stock Disponible</h3>
-            {stock}
+  return (
+    <>
+      <div className="ItemCount">
+      <h5 className="card-title fw-bolder"> {nombre} </h5>
+        <img src="" alt="" />
+        <div className="counter">{counter}</div>
+        <div className="buttons">
+          <button className="decremento" onClick={decremento}>
+            -
+          </button>
+          <button className="incremento" onClick={incremento}>
+            +
+          </button>
         </div>
-    )
+        <h3>Stock Disponible</h3>
+        {stock}
+        <Link to={"/producto/" + id } ><button type="button" className="btn card-btn-cart mt-3 btn-sm btn-add">Examinar</button></Link>
+      </div>
+    </>
+  )
 }
 
-export default ItemCount
+export default ItemCount;
